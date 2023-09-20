865bcc4655b21e8caa563fbeb1144001fa5fa4ba
Franco_Serjen <francoserjen100@gmail.com>
2023-09-20 11:08:18 -0300

    Modificacion de valores de dados de manera individual

--- src/app/components/dice/dice.component.ts
+++ src/app/components/dice/dice.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ColorsIdsEnum } from 'src/app/models/colors-ids.enum';
 import { ColorsNameEnum } from 'src/app/models/colors-names.enum';
+import { Dice } from 'src/app/models/dice.interface';
 
 @Component({
   selector: 'app-dice',
@@ -12,4 +13,13 @@ export class DiceComponent {
     colorName: ColorsNameEnum;
     dicesValues: number[];
   };
+
+
+  arrayOpciones(min: number, max: number) {
+    let numeros = [];
+    for (let i = min; i <= max; i++) {
+      numeros.push(i);
+    }
+    return numeros;
+  }
 }
